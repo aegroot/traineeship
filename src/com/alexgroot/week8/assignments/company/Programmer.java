8837cc1a5d0e0a242b9b4bacab97ff1bf5274c8d
aegroot <alex-groot@live.nl>
2023-04-19 11:30:44 +0200

    narrative rest

---
+++ src/com/alexgroot/week8/assignments/company/Programmer.java
@@ -0,0 +1,7 @@
+package com.alexgroot.week8.assignments.company;
+
+public class Programmer extends Employee {
+    public Programmer(String name, String adress, String phoneNumber, int experience) {
+        super(name, adress, phoneNumber, experience);
+    }
+}
