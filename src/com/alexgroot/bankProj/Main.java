827b5c5b0b2bdbc2ed815f9239f4afb2586fff14
aegroot <alex-groot@live.nl>
2023-07-17 21:45:47 +0200

    bank finish v2

--- src/com/alexgroot/bankProj/Main.java
+++ src/com/alexgroot/bankProj/Main.java
@@ -5,12 +5,14 @@ import com.alexgroot.bankProj.domain.User;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
 
         User henk = new User(new BankAccount("22"), "henk", "vlierdam", new Date());
         User tom = new User(new BankAccount("23"), "tom", "java", new Date());
@@ -19,45 +21,53 @@ public class Main {
         User.transaction(new BigDecimal(200), henk.getBankAccount(), tom.getBankAccount());
 
         Optional<BankAccount> accountOptional = chooseCustomer(sc);
+        do {
+            if (accountOptional.isEmpty()) {
+                return;
+            }
+            BankAccount account = accountOptional.get();
+            System.out.println("saldo: " + account.calculateBalance());
+            System.out.println("Optie 1: storten\n" +
+                    "Optie 2: overboeken\n" +
+                    "Optie 3: transactiegeschiedenis inzien.\n" +
+                    "Optie 4: terug naar klant kiezen. \n"
+                    + "Optie 5: stoppen. ");
 
 
-        System.out.println(" optie 1: storten\n" +
-                "Optie 2: overboeken\n" +
-                "Optie 3: transactiegeschiedenis inzien.\n" +
-                "OPtie 4: terug naar klant kiezen");
+            int choice = sc.nextInt();
 
-        int choice = sc.nextInt();
-        if (accountOptional.isEmpty()) {
-            return;
-        }
-        BankAccount account = accountOptional.get();
 
-        switch (choice) {
-            case 1:
-                System.out.println("gaat uw gang");
-                account.stort(sc.nextBigDecimal());
-                break;
-            case 2:
-                System.out.println("welk banknummer?");
-                sc.nextLine();
-                Optional<BankAccount> acc2 = chooseCustomer(sc);
-                if (acc2.isEmpty()) {
-                    System.out.println("user does not exist");
+            switch (choice) {
+                case 1:
+                    System.out.println("gaat uw gang");
+                    account.stort(sc.nextBigDecimal());
                     break;
-                }
-                System.out.println("hoeveel?");
-                int amount = sc.nextInt();
-                if (amount > 0) {
-                    User.transaction(BigDecimal.valueOf(amount), acc2.get(), account);
-                } else {
-                    System.out.println("bad transaction");
-                }
-                break;
-            case 3:
-                System.out.println(account.getTransactions());
-            case 4:
-                break;
+                case 2:
+                    System.out.println("welke klant?");
+                    sc.next();
+                    Optional<BankAccount> acc2 = chooseCustomer(sc);
+                    if (acc2.isEmpty()) {
+                        System.out.println("user does not exist");
+                        break;
+                    }
+                    System.out.println("hoeveel?");
+                    BigDecimal amount = sc.nextBigDecimal();
+                    if (amount.doubleValue() > 0) {
+                        User.transaction(amount, acc2.get(), account);
+                    } else {
+                        System.out.println("bad transaction");
+                    }
+                    break;
+                case 3:
+                    System.out.println(account.getTransactions());
+                case 4:
+                    break;
+                case 5:
+                    return;
+            }
+
         }
+        while (true);
 
 
     }
@@ -68,9 +78,24 @@ public class Main {
         Optional<BankAccount> account = Optional.empty();
 
         do {
-            System.out.println(" Van welk persoon of rekening wilt u de gegevens hebben?");
-            String name = sc.nextLine();
-            account = User.getUserByName(name);
+            System.out.println("1. Van welk persoon wilt u de gegevens hebben" +
+                    "\n2. van welke rekening wilt u de gegevens hebben? " +
+                    "\n3. stoppen met klant kiezen");
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            if (choice == 1) {
+                System.out.println("please enter the name");
+                String name = sc.nextLine();
+                account = User.getUserByName(name);
+            } else if (choice == 2) {
+                System.out.println("please enter the bank number ");
+                String number = sc.nextLine();
+                account = User.getUserByNumber(number);
+            } else if (choice == 3) {
+                break;
+            }
+
         }
         while (account.isEmpty());
 
